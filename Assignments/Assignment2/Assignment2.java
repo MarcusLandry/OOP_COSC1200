406b655cf3fed2ec318ae268458da8475c0f2e59
MarcusLandry <72230603+MarcusLandry@users.noreply.github.com>
2024-06-21 12:52:40 -0400

    Final commit
    finished the code.

--- Assignments/Assignment2/Assignment2.java
+++ Assignments/Assignment2/Assignment2.java
@@ -30,9 +30,10 @@ public class Assignment2 {
         for (int day = 0; day < totalDays; day++) {
             boolean validInput = false;
             while (!validInput) {
+                System.out.printf("Day %d:\n", day + 1);
                 System.out.println("Enter lowest temperature of the day (-30 to 30): ");
                 double lowTemp = scanner.nextDouble();
-                System.out.println("Enter lowest temperature of the day (-30 to 30): ");
+                System.out.println("Enter highest temperature of the day (-30 to 30): ");
                 double highTemp = scanner.nextDouble();
 
                 if (lowTemp >= -30 && lowTemp <= 30 && highTemp >= -30 && highTemp <= 30 && lowTemp <= highTemp) {
@@ -40,16 +41,37 @@ public class Assignment2 {
                     highTemperature[day] = highTemp;
                     validInput = true;
                 } else {
-                    System.out.println("Invalid input. Please enter a number between -30 and 30");
+                    System.out.println("Invalid input. Please enter a number between -30 and 30 \n" +
+                            "for both temperatures, and ensure low temperature is not higher than high temperature.");
                 }
 
             }
 
-            
+            // calculating the temp for the day
+            double averageDayTemperature = (lowTemperature[day] + highTemperature[day]) / 2;
+            System.out.printf("Average temperature for day %d: %.1f\n", day + 1, averageDayTemperature);
+
+            // updates the lists of averages
+            overallAverage += averageDayTemperature;
+            if (lowTemperature[day] < lowestTemp) {
+                lowestTemp = lowTemperature[day];
+                overallLowestTempDay = day + 1;
+            }
+
+            if (highTemperature[day] > highestTemp) {
+                highestTemp = highTemperature[day];
+                overallHighestTempDay = day + 1;
+            }
 
         }
 
+        // total avg temp
+        overallAverage /= totalDays;
 
+        // outputs final results to console
+        System.out.printf("\nOverall Average Temperature: %.1f", overallAverage);
+        System.out.printf("\nDay with the lowest temperature: %d", overallLowestTempDay);
+        System.out.printf("\nDay with the highest temperature: %d", overallHighestTempDay);
 
 
     }
