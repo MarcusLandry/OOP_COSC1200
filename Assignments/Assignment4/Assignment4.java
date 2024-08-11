b64518e8f13da39cce073f9011bb7a059778f07d
MarcusLandry <72230603+MarcusLandry@users.noreply.github.com>
2024-08-11 06:52:10 -0400

    product class started

---
+++ Assignments/Assignment4/Assignment4.java
@@ -0,0 +1,37 @@
+import java.text.DecimalFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+// class for Products
+class Product {
+    private int sku;
+    private String name;
+    private double unitCost;
+    private int unitsOnHand;
+    private int unitsNeeded;
+    private String specialInstructions;
+
+    // Default constructor for Products
+    public Product() {
+        this.sku = 0;
+        this.name = "";
+        this.unitCost = 0.0;
+        this.unitsOnHand = 0;
+        this.unitsNeeded = 0;
+        this.specialInstructions = "";
+    }
+
+
+
+}
+
+public class Assignment4 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
